04a77cd5e2d5cbfeec8123edc26158ed1cdd1393
Anthony Minchenko <code.inside.us@gmail.com>
2020-10-16 02:46:57 +0300

    Created ApplicationDbContext

---
+++ ShowerQ/Models/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShowerQ.Models.Entities;
+using ShowerQ.Models.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowerQ.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        #region Users
+        public DbSet<Tenant> Tenant { get; set; }
+        public DbSet<DormitoryAdministrator> DormitoryAdministrators { get; set; }
+        public DbSet<SystemAdministrator> SystemAdministrators { get; set; }
+        #endregion
+
+        #region "Other entities"
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Dormitory> Dormitories { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<University> Universities { get; set; }
+        #endregion
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- ShowerQ/Startup.cs
+++ ShowerQ/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ShowerQ.Models;
 
 namespace ShowerQ
 {
@@ -25,6 +27,9 @@ namespace ShowerQ
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connection));
             services.AddControllers();
         }
 
