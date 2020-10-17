d4543081029058944947239d239b399b757c1cac
Anthony Minchenko <code.inside.us@gmail.com>
2020-10-17 04:37:06 +0300

    Created cities, universities and dormitories controllers

---
+++ ShowerQ/Controllers/DormitoriesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShowerQ.Models;
+using ShowerQ.Models.Entities;
+
+namespace ShowerQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DormitoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DormitoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Dormitories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Dormitory>>> GetDormitories()
+        {
+            return await _context.Dormitories.ToListAsync();
+        }
+
+        // GET: api/Dormitories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Dormitory>> GetDormitory(int id)
+        {
+            var dormitory = await _context.Dormitories.FindAsync(id);
+
+            if (dormitory == null)
+            {
+                return NotFound();
+            }
+
+            return dormitory;
+        }
+
+        // PUT: api/Dormitories/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDormitory(int id, Dormitory dormitory)
+        {
+            if (id != dormitory.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(dormitory).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DormitoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Dormitories
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Dormitory>> PostDormitory(Dormitory dormitory)
+        {
+            var schedule = CreateNewSchedule();
+
+            dormitory.CurrentScheduleId = schedule.Id;
+            dormitory.CurrentSchedule = schedule;
+
+            _context.Dormitories.Add(dormitory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDormitory", new { id = dormitory.Id }, dormitory);
+        }
+
+        private Schedule CreateNewSchedule()
+        {
+            var schedule = new Schedule();
+            schedule.TenantsPerInterval = 1;
+            return _context.Schedules.Add(schedule).Entity;
+        }
+
+        // DELETE: api/Dormitories/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Dormitory>> DeleteDormitory(int id)
+        {
+            var dormitory = await _context.Dormitories.FindAsync(id);
+            if (dormitory == null)
+            {
+                return NotFound();
+            }
+
+            _context.Dormitories.Remove(dormitory);
+            await _context.SaveChangesAsync();
+
+            return dormitory;
+        }
+
+        private bool DormitoryExists(int id)
+        {
+            return _context.Dormitories.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ShowerQ/Controllers/UniversitiesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShowerQ.Models;
+using ShowerQ.Models.Entities;
+
+namespace ShowerQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UniversitiesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UniversitiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Universities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<University>>> GetUniversities()
+        {
+            return await _context.Universities.ToListAsync();
+        }
+
+        // GET: api/Universities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<University>> GetUniversity(int id)
+        {
+            var university = await _context.Universities.FindAsync(id);
+
+            if (university == null)
+            {
+                return NotFound();
+            }
+
+            return university;
+        }
+
+        // PUT: api/Universities/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUniversity(int id, University university)
+        {
+            if (id != university.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(university).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UniversityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Universities
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<University>> PostUniversity(University university)
+        {
+            _context.Universities.Add(university);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUniversity", new { id = university.Id }, university);
+        }
+
+        // DELETE: api/Universities/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<University>> DeleteUniversity(int id)
+        {
+            var university = await _context.Universities.FindAsync(id);
+            if (university == null)
+            {
+                return NotFound();
+            }
+
+            _context.Universities.Remove(university);
+            await _context.SaveChangesAsync();
+
+            return university;
+        }
+
+        private bool UniversityExists(int id)
+        {
+            return _context.Universities.Any(e => e.Id == id);
+        }
+    }
+}
