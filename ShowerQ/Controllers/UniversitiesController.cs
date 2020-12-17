d9c4e7af908569bc0041b2c9e6cba2fa92a0640b
Anthony Minchenko <65635298+anthony-mi@users.noreply.github.com>
2020-12-18 01:22:22 +0200

    Updated `UniversitiesController`

--- ShowerQ/Controllers/UniversitiesController.cs
+++ ShowerQ/Controllers/UniversitiesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShowerQ.Models;
 using ShowerQ.Models.Entities;
+using ShowerQ.Models.Entities.Validators;
 
 namespace ShowerQ.Controllers
 {
@@ -23,14 +24,22 @@ namespace ShowerQ.Controllers
 
         // GET: api/Universities
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<University>>> GetUniversities()
+        public async Task<ActionResult<IEnumerable<object>>> GetUniversities()
         {
-            return await _context.Universities.ToListAsync();
+            return _context.Universities
+                .Select(
+                university => new
+                {
+                    id = university.Id,
+                    name = university.Name,
+                    cityId = university.CityId,
+                    dormitories = university.Dormitories.Select(d => d.Id)
+                }).ToArray();
         }
 
         // GET: api/Universities/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<University>> GetUniversity(int id)
+        public async Task<ActionResult<object>> GetUniversity(int id)
         {
             var university = await _context.Universities.FindAsync(id);
 
@@ -39,7 +48,13 @@ namespace ShowerQ.Controllers
                 return NotFound();
             }
 
-            return university;
+            return new
+            {
+                id = university.Id,
+                name = university.Name,
+                cityId = university.CityId,
+                dormitories = university.Dormitories.Select(d => d.Id)
+            };
         }
 
         // PUT: api/Universities/5
@@ -51,6 +66,15 @@ namespace ShowerQ.Controllers
             if (id != university.Id)
             {
                 return BadRequest();
+            }
+
+            UniversityValidator validator = new(_context);
+
+            var result = validator.Validate(university);
+
+            if (!result.IsValid)
+            {
+                return StatusCode(500, new { errors = result.Errors });
             }
 
             _context.Entry(university).State = EntityState.Modified;
@@ -78,12 +102,26 @@ namespace ShowerQ.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<University>> PostUniversity(University university)
+        public async Task<ActionResult<object>> PostUniversity(University university)
         {
+            UniversityValidator validator = new(_context);
+
+            var result = validator.Validate(university);
+
+            if (!result.IsValid)
+            {
+                return StatusCode(500, new { errors = result.Errors });
+            }
+
             _context.Universities.Add(university);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUniversity", new { id = university.Id }, university);
+            return new
+            {
+                id = university.Id,
+                name = university.Name,
+                cityId = university.CityId
+            };
         }
 
         // DELETE: api/Universities/5
@@ -99,7 +137,7 @@ namespace ShowerQ.Controllers
             _context.Universities.Remove(university);
             await _context.SaveChangesAsync();
 
-            return university;
+            return Ok();
         }
 
         private bool UniversityExists(int id)
