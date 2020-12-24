cc1e5b41d764c5ea2618ee4cdfe9feb44a40d6db
Anthony Minchenko <65635298+anthony-mi@users.noreply.github.com>
2020-12-24 07:03:53 +0200

    Created extension for `IdentityUser`

---
+++ ShowerQ/Extensions/IdentityUserExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ShowerQ.Models.Entities.Users;
+using System.Linq;
+using System.Reflection;
+
+namespace ShowerQ.Extensions
+{
+    public static class IdentityUserExtension
+    {
+        public static IdentityUser Initialize(this IdentityUser user, TenantModel tenantModel)
+        {
+            var tenantModelProperties = typeof(TenantModel).GetProperties();
+            var identityUserProperties = typeof(IdentityUser).GetProperties();
+
+            foreach (var tmProperty in tenantModelProperties)
+            {
+                var value = tmProperty.GetValue(tenantModel);
+                var iuProperty = identityUserProperties.FirstOrDefault(prop => prop.Name.Equals(tmProperty.Name));
+
+                if(iuProperty is not default(PropertyInfo))
+                {
+                    iuProperty.SetValue(user, value);
+                }
+            }
+
+            return user;
+        }
+    }
+}
